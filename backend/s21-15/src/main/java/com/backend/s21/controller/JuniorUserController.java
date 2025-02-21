f5905134f52a43fb99e68db9d83fd8f1118c3d86
Omar3636 <yairmenaz92@gmail.com>
2025-02-20 21:28:52 -0300

    feat(request): Created method for registration of Junior User.

--- backend/s21-15/src/main/java/com/backend/s21/controller/JuniorUserController.java
+++ backend/s21-15/src/main/java/com/backend/s21/controller/JuniorUserController.java
@@ -1,9 +1,18 @@
 package com.backend.s21.controller;
 
+import com.backend.s21.model.dto.junior.JuniorUserDTO;
+import com.backend.s21.model.users.JuniorUser;
 import com.backend.s21.repository.JuniorUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/junior")
@@ -12,4 +21,11 @@ public class JuniorUserController {
     @Autowired
     private JuniorUserRepository juniorRepository;
 
+    @PostMapping
+    public ResponseEntity<JuniorUserDTO> registerJuniorUser(@RequestBody @Validated JuniorUser juniorUser, UriComponentsBuilder uriComponentsBuilder) {
+        JuniorUser juniorUser1 = juniorRepository.save(juniorUser);
+        JuniorUserDTO juniorUserDto = new JuniorUserDTO(juniorUser);
+        URI url = uriComponentsBuilder.path("/junior/{nickname}").buildAndExpand(juniorUserDto.getNickname()).toUri();
+        return ResponseEntity.created(url).body(juniorUserDto);
+    }
 }
