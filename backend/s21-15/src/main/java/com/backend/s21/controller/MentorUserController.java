cfae44f427f6d2b3ec337a1adda0b3af4336d63a
Omar3636 <yairmenaz92@gmail.com>
2025-02-21 23:05:36 -0300

    feat(register): Created methods for register and show mentor user info.

--- backend/s21-15/src/main/java/com/backend/s21/controller/MentorUserController.java
+++ backend/s21-15/src/main/java/com/backend/s21/controller/MentorUserController.java
@@ -1,9 +1,14 @@
 package com.backend.s21.controller;
 
+import com.backend.s21.model.users.MentorUser;
 import com.backend.s21.repository.MentorUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/mentor")
@@ -12,4 +17,19 @@ public class MentorUserController {
     @Autowired
     private MentorUserRepository mentorRepository;
 
+    //Queda pendiente la respuesta a devolver con un MentorUserDTO para mejorar la seguridad.
+    @PostMapping
+    public ResponseEntity<MentorUser> registerJuniorUser(@RequestBody @Validated MentorUser mentorUser, UriComponentsBuilder uriComponentsBuilder) {
+        MentorUser mentorUser1 = mentorRepository.save(mentorUser);
+        URI url = uriComponentsBuilder.path("/mentor/{nickname}").buildAndExpand(mentorUser1.getNickname()).toUri();
+        return ResponseEntity.created(url).body(mentorUser1);
+    }
+
+    //Obtener información de un Usuario Mentor a traves de una dirección PATH.
+    //Pendiente devolver info como Dto por seguridad.
+    @GetMapping("/{nickname}")
+    public ResponseEntity<MentorUser> mostrarUsuario(@PathVariable String nickname) {
+        MentorUser user = mentorRepository.getReferenceByNickname(nickname);
+        return ResponseEntity.ok(user);
+    }
 }
--- backend/s21-15/src/main/java/com/backend/s21/repository/MentorUserRepository.java
+++ backend/s21-15/src/main/java/com/backend/s21/repository/MentorUserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MentorUserRepository extends JpaRepository<MentorUser, Long> {
 
+    MentorUser getReferenceByNickname(String nickname);
 }
