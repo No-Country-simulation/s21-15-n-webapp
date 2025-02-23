05f3034c777e903c5defc9e493dea2d60def1a1c
Omar3636 <yairmenaz92@gmail.com>
2025-02-22 23:52:48 -0300

    feat(CRUD): Created methods to create courses and challenges.

--- backend/s21-15/src/main/java/com/backend/s21/controller/MentorUserController.java
+++ backend/s21-15/src/main/java/com/backend/s21/controller/MentorUserController.java
@@ -1,9 +1,12 @@
 package com.backend.s21.controller;
 
+import com.backend.s21.model.dto.junior.ChallengeDTO;
 import com.backend.s21.model.dto.junior.CourseDTO;
+import com.backend.s21.model.learningPath.Challenge;
 import com.backend.s21.model.learningPath.Course;
 import com.backend.s21.model.users.MentorUser;
 import com.backend.s21.model.users.User;
+import com.backend.s21.repository.ChallengeRepository;
 import com.backend.s21.repository.CourseRepository;
 import com.backend.s21.repository.MentorUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +27,12 @@ public class MentorUserController {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private ChallengeRepository challengeRepository;
+
     //Queda pendiente la respuesta a devolver con un MentorUserDTO para mejorar la seguridad.
     @PostMapping
-    public ResponseEntity<MentorUser> registerJuniorUser(@RequestBody @Validated MentorUser mentorUser, UriComponentsBuilder uriComponentsBuilder) {
+    public ResponseEntity<MentorUser> registerMentorUser(@RequestBody @Validated MentorUser mentorUser, UriComponentsBuilder uriComponentsBuilder) {
         MentorUser mentorUser1 = mentorRepository.save(mentorUser);
         URI url = uriComponentsBuilder.path("/mentor/{nickname}").buildAndExpand(mentorUser1.getNickname()).toUri();
         return ResponseEntity.created(url).body(mentorUser1);
@@ -35,7 +41,7 @@ public class MentorUserController {
     //Obtener información de un Usuario Mentor a traves de una dirección PATH.
     //Pendiente devolver info como Dto por seguridad.
     @GetMapping("/{nickname}")
-    public ResponseEntity<MentorUser> mostrarUsuario(@PathVariable String nickname) {
+    public ResponseEntity<MentorUser> showUser(@PathVariable String nickname) {
         MentorUser user = mentorRepository.getReferenceByNickname(nickname);
         return ResponseEntity.ok(user);
     }
@@ -48,5 +54,13 @@ public class MentorUserController {
         CourseDTO courseDTO = new CourseDTO(course);
         URI url = uriComponentsBuilder.path("/course/{id}").buildAndExpand(courseDTO.getId()).toUri();
         return ResponseEntity.created(url).body(courseDTO);
+    }
+
+    @PostMapping("/{nickname}/createchallenge")
+    public ResponseEntity<ChallengeDTO> createChallenge(@RequestBody @Validated Challenge challengeinfo, @PathVariable String nickname, UriComponentsBuilder uriComponentsBuilder) {
+        Challenge challenge = challengeRepository.save(challengeinfo);
+        ChallengeDTO challengeDTO = new ChallengeDTO(challenge);
+        URI url = uriComponentsBuilder.path("/challenge/{id}").buildAndExpand(challengeDTO.getId()).toUri();
+        return ResponseEntity.created(url).body(challengeDTO);
     }
 }
