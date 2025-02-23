690bc44a4f4f68ef2f2a6c0b9d748903c958f6d7
Omar3636 <yairmenaz92@gmail.com>
2025-02-22 23:50:54 -0300

    feat(CRUD): Created methods to link social network and see a list of challange history & course history.

--- backend/s21-15/src/main/java/com/backend/s21/controller/JuniorUserController.java
+++ backend/s21-15/src/main/java/com/backend/s21/controller/JuniorUserController.java
@@ -1,9 +1,13 @@
 package com.backend.s21.controller;
 
-import com.backend.s21.model.dto.junior.JuniorUserDTO;
+import com.backend.s21.model.dto.junior.*;
 import com.backend.s21.model.users.JuniorUser;
-import com.backend.s21.repository.JuniorUserRepository;
+import com.backend.s21.model.users.SocialNetwork;
+import com.backend.s21.model.users.User;
+import com.backend.s21.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +22,18 @@ public class JuniorUserController {
     @Autowired
     private JuniorUserRepository juniorRepository;
 
+    @Autowired
+    private ChallengeRepository challengeRepository;
+
+    @Autowired
+    private ChallengeHistoryRepository challengeHRepository;
+
+    @Autowired
+    private SocialNetworkRepository socialNRepository;
+
+    @Autowired
+    private CourseHistoryRepository courseHRepository;
+
     @PostMapping
     public ResponseEntity<JuniorUserDTO> registerJuniorUser(@RequestBody @Validated JuniorUser juniorUser, UriComponentsBuilder uriComponentsBuilder) {
         JuniorUser juniorUser1 = juniorRepository.save(juniorUser);
@@ -28,9 +44,45 @@ public class JuniorUserController {
 
     //Obtener información de un Usuario Junior a traves de una dirección PATH.
     @GetMapping("/{nickname}")
-    public ResponseEntity<JuniorUserDTO> mostrarUsuario(@PathVariable String nickname) {
-        JuniorUser user = juniorRepository.getReferenceByNickname(nickname);
-        return ResponseEntity.ok(new JuniorUserDTO(user));
+    public ResponseEntity<JuniorUserDTO> showUser(@PathVariable String nickname) {
+        try {
+            JuniorUser user = juniorRepository.getReferenceByNickname(nickname);
+            return ResponseEntity.ok(new JuniorUserDTO(user));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    //No funcional, falta implementar un metodo en @Service o contructor en @Entity para ligar el Usuario a la red Social antes de persistir en BD.
+    @PostMapping("/{nickname}/socialnetworks")
+    public ResponseEntity<SocialNetworkDTO> linkSocialNetwork(@RequestBody @Validated SocialNetwork infoSocialNet, @PathVariable String nickname) {
+        try {
+            User user = juniorRepository.getReferenceByNickname(nickname);
+            SocialNetwork socialNetwork = socialNRepository.save(infoSocialNet);
+            socialNetwork.setUser(user);
+            return ResponseEntity.ok(new SocialNetworkDTO(socialNetwork));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
+    @GetMapping("/{nickname}/challengehistory")
+    public ResponseEntity<Page<ChallengeHistoryDTO>> listChallengeHistory(@PathVariable String nickname, Pageable pageable) {
+        try {
+            return ResponseEntity.ok(challengeHRepository.findAll(pageable).map(ChallengeHistoryDTO::new));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/{nickname}/coursehistory")
+    public ResponseEntity<Page<CourseHistoryDTO>> listCourseHistory(@PathVariable String nickname, Pageable pageable) {
+        try {
+            return ResponseEntity.ok(courseHRepository.findAll(pageable).map(CourseHistoryDTO::new));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
