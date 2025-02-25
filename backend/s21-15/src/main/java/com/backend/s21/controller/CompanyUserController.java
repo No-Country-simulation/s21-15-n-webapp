39f8886300314bf75714c1e39c4030993103690f
Omar3636 <yairmenaz92@gmail.com>
2025-02-24 23:31:44 -0300

    feat(CRUD): Created endpoints to create Company User and show the user.

--- backend/s21-15/src/main/java/com/backend/s21/controller/CompanyUserController.java
+++ backend/s21-15/src/main/java/com/backend/s21/controller/CompanyUserController.java
@@ -1,15 +1,40 @@
 package com.backend.s21.controller;
 
+import com.backend.s21.model.users.CompanyUser;
 import com.backend.s21.repository.CompanyUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/company")
 public class CompanyUserController {
 
     @Autowired
-    CompanyUserRepository companyRepository;
+    private CompanyUserRepository companyRepository;
 
+    @PostMapping
+    public ResponseEntity<CompanyUser> registerCompanyUser(@RequestBody @Validated CompanyUser infoCompany, UriComponentsBuilder uri) {
+        CompanyUser companyUser = companyRepository.save(infoCompany);
+        URI url = uri.path("/company/{nickname}").buildAndExpand(companyUser.getNickname()).toUri();
+        return ResponseEntity.created(url).body(companyUser);
+    }
+
+    @GetMapping("/{nickname}")
+    public ResponseEntity<CompanyUser> showCompanyUser(@PathVariable String nickname){
+        try {
+            CompanyUser user = companyRepository.getReferenceByNickname(nickname);
+            if (user != null) {
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
--- backend/s21-15/src/main/java/com/backend/s21/repository/CompanyUserRepository.java
+++ backend/s21-15/src/main/java/com/backend/s21/repository/CompanyUserRepository.java
@@ -4,4 +4,6 @@ import com.backend.s21.model.users.CompanyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyUserRepository extends JpaRepository<CompanyUser, Long> {
+
+    CompanyUser getReferenceByNickname(String nickname);
 }
