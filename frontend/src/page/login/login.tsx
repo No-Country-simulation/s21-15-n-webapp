1857a87229a6361b345560c4be845b64247ec09b
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2025-02-25 19:22:01 -0300

    update logo

--- frontend/src/page/login/login.tsx
+++ frontend/src/page/login/login.tsx
@@ -2,7 +2,13 @@
 const Login = () => {
   return (
     <div className="h-screen w-screen">
-        <div className=""></div>
+        <div className="bg-[#000115] w-1/2 h-screen rounded-r-3xl">
+          <a href=""><img src="" alt="" /> StartPerks</a>
+          <div>
+            <h2>Impulsa tu carrera en tecnología</h2>
+            <p>Únete a StartPerks y transforma tu futuro profesional a través de simulaciones laborales y mentorías</p>
+          </div> 
+        </div>
         <div className=""></div>
     </div>
   )
