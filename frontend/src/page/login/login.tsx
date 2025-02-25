ad50327850240044c81b37c52f2693691c8f57b1
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2025-02-25 19:04:07 -0300

    Update route

---
+++ frontend/src/page/login/login.tsx
@@ -0,0 +1,11 @@
+
+const Login = () => {
+  return (
+    <div className="h-screen w-screen">
+        <div className=""></div>
+        <div className=""></div>
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/src/page/login/register.tsx
@@ -0,0 +1,11 @@
+
+const Register = () => {
+  return (
+    <div className="h-screen w-screen">
+        <div className=""></div>
+        <div className=""></div>
+    </div>
+  )
+}
+
+export default Register
