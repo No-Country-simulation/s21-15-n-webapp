c76c68c032ecc37969adfcf99899f836d84b2631
0303 <wilson.osorio@uptc.edu.co>
2025-02-24 20:35:22 -0500

    ♻️ refactor(CRUD security): optimize logic of CRUD

--- backend/s21-15/src/main/java/com/backend/s21/security/WebSecurityConfig.java
+++ backend/s21-15/src/main/java/com/backend/s21/security/WebSecurityConfig.java
@@ -1,25 +1,16 @@
 package com.backend.s21.security;
 
 import com.backend.s21.security.jwt.JwtAuthenticationConverter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -33,38 +24,32 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity
-                .csrf(AbstractHttpConfigurer::disable) // Deshabilita CSRF para API stateless
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        // Rutas permitidas sin autenticación
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
                                 "/swagger-ui.html", "/webjars/**",
                                 "/api/keycloud/login", "/api/keycloud/create").permitAll()
                         .anyRequest().authenticated() // Todas las demás requieren autenticación
                 )
 
-                // Configuración de OAuth2 para login con redes sociales
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/oauth2/authorization/google") // Página de login con Google
                         .defaultSuccessUrl("/api/user/info", true) // Redirección después de login exitoso
                         .failureUrl("/login?error=true") // Redirección si falla el login
                 )
 
-                // Configuración de OAuth2 para validación con JWT (cuando ya tiene token)
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter))
                 )
 
-                // Manejo de excepciones
                 .exceptionHandling(exceptions -> exceptions
                         .authenticationEntryPoint((request, response, authException) ->
                                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
                         )
                 )
 
-                // Deshabilita formulario de login porque usamos OAuth2 y JWT
                 .formLogin(AbstractHttpConfigurer::disable)
 
-                // Define la política de sesiones como STATELESS (para JWT)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build();
--- backend/s21-15/src/main/java/com/backend/s21/security/service/impl/KeyCloakServiceImpl.java
+++ backend/s21-15/src/main/java/com/backend/s21/security/service/impl/KeyCloakServiceImpl.java
@@ -2,62 +2,254 @@ package com.backend.s21.security.service.impl;
 
 import com.backend.s21.security.jwt.JWTResponse;
 import com.backend.s21.security.service.IKeyCloakService;
+import com.backend.s21.security.util.KeyCloak;
 import com.backend.s21.security.util.model.UserRecord;
+import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.RoleResource;
+import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 public class KeyCloakServiceImpl implements IKeyCloakService {
 
-    private final IKeyCloakService keyService;
+    private final KeyCloak keyService;
     private RestTemplate restTemplate;
+    private UserRecord userRecord;
+    @Value("${keycloak.client-id}")
+    private String clientId;
+    @Value("${keycloak.client-secret}")
+    private String clientSecret;
+    @Value("${keycloak.token-url}")
+    private String tokenUrl;
 
     @Override
     public List<UserRepresentation> findAllUsers() {
-        return keyService.findAllUsers().stream().toList();
+        try {
+            return keyService.getUserResource().list();
+        } catch (Exception e) {
+            throw new RuntimeException("Error to get all users");
+        }
     }
 
     @Override
     public UserRepresentation searchUserByUsername(String username) {
-        return keyService.searchUserByUsername(username);
+        try {
+            return keyService
+                    .getUserResource()
+                    .search(username)
+                    .stream().findFirst()
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+        } catch (Exception e) {
+            throw new RuntimeException("Error to get user by username");
+        }
     }
 
     @Override
-    public String createUser(UserRecord userRecord) {
+    public String createUser(UserRecord user) {
 
-        return "";
+        UsersResource usersResource = keyService.getUserResource();
+        UserRepresentation userRepresentation = getUserRepresentation(user);
+
+        try (Response response = usersResource.create(userRepresentation)) {
+            int status = response.getStatus();
+
+            if (status == 201) {
+                String path = response.getLocation().getPath();
+                String idUser = path.substring(path.lastIndexOf("/") + 1);
+
+                // Asignar la contraseña al usuario
+                CredentialRepresentation credentialRepresentation = getCredentialRepresentation(user);
+                usersResource.get(idUser)
+                        .resetPassword(credentialRepresentation);
+
+                RealmResource realmResource = keyService.getRealmResource();
+                List<RoleRepresentation> rolesRepresentation = null;
+                // Asignar roles al usuario
+                assignRoles(user, realmResource, idUser);
+                UserRepresentation representation = usersResource.get(idUser).toRepresentation();
+                log.info("Id creacion de usuario"+ representation.getId());
+
+                return "User created successfully!!";
+            } else if (status == 409) {
+                log.error("User already exists!");
+                return "User already exists!";
+            } else {
+                log.error("Error creating user, please contact the administrator.");
+                return "Error creating user, please contact the administrator.";
+            }
+        }
     }
 
-    @Override
+    private static void assignRoles(UserRecord user, RealmResource realmResource, String idUser) {
+        List<RoleRepresentation> rolesRepresentation;
+        if (user.roles() == null || user.roles().isEmpty() ) {
+            rolesRepresentation = List.of(realmResource.roles().get("user").toRepresentation()); //default role
+        } else {
+            rolesRepresentation = realmResource.roles()
+                    .list()
+                    .stream()
+                    .filter(role -> user.roles()
+                            .stream()
+                            .anyMatch(roleName -> roleName.equalsIgnoreCase(role.getName())))
+                    .toList();
+        }
+        realmResource.users()
+                .get(idUser)
+                .roles().realmLevel()
+                .add(rolesRepresentation);
+    }
+
+    private static UserRepresentation getUserRepresentation(UserRecord user) {
+        UserRepresentation userRepresentation = new UserRepresentation();
+        userRepresentation.setFirstName(user.firstName());
+        userRepresentation.setLastName(user.lastName());
+        userRepresentation.setEmail(user.email());
+        userRepresentation.setUsername(user.username());
+        userRepresentation.setEnabled(true);
+        userRepresentation.setEmailVerified(true);
+        return userRepresentation;
+    }
+
     public JWTResponse login(String username, String password) {
-        return null;
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", "password");
+        map.add("client_id", clientId);
+        map.add("client_secret", clientSecret);
+        map.add("username", username);
+        map.add("password", password);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        try {
+            ResponseEntity<JWTResponse> response = restTemplate.postForEntity(tokenUrl, request, JWTResponse.class);
+            if (response.getStatusCode() == HttpStatus.OK) {
+                JWTResponse responseBody = response.getBody();
+                if (responseBody != null) {
+                    return responseBody;
+                } else {
+                    throw new RuntimeException("Response body is null");
+                }
+            } else {
+                throw new RuntimeException("Invalid credentials");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to login", e);
+        }
     }
 
+    @Transactional
     @Override
     public void deleteUser(String username) {
+        try {
+            String userId = searchUserByUsername(username).getId();
+            if (userId != null) {
+                keyService.getUserResource()
+                        .get(userId)
+                        .remove();
+//                userService.deleteByIdKeycloak(userId);
+                log.info("User {} deleted successfully", username);
+            }
+        } catch (RuntimeException e) {
+            log.error("Error deleting user {}: {}", username, e.getMessage());
+            throw new RuntimeException("Error deleting user: " + e.getMessage(), e);
+        }
+    }
 
+    @Override
+    public void updateUser(UserRecord user) {
+        try {
+            // Buscar usuario por nombre de usuario
+            UserRepresentation existingUser = searchUserByUsername(user.username());
+//            String idUserDB = userService.findByIdKeycloak(existingUser.getId()).getIdKeycloak();
+            if (existingUser == null) {
+                throw new RuntimeException("User not found: " + user.username());
+            }
+            CredentialRepresentation credentialRepresentation = getCredentialRepresentation(user);
+            UserRepresentation userRepresentation = getUserRepresentation(user);
+            userRepresentation.setCredentials(List.of(credentialRepresentation));
+            UserResource usersResource = keyService.getUserResource().get(existingUser.getId());
+            assignRoles(user, keyService.getRealmResource(), existingUser.getId());
+//            builders = com.back.model.entities.User.builder()
+//                    .name(user.firstName())
+//                    .email(user.email())
+//                    .idKeycloak(userRepresentation.getId())
+//                    .roles(user.roles())
+//                    .build();
+//            userService.updateUser(idUserDB, builders);
+            usersResource.update(userRepresentation);
+
+            log.info("User {} updated successfully", user.username());
+        } catch (RuntimeException e) {
+            log.error("Error updating user {}: {}", user.username(), e.getMessage());
+            throw new RuntimeException("Error updating user: " + e.getMessage(), e);
+        }
+    }
+
+    private static CredentialRepresentation getCredentialRepresentation(UserRecord user) {
+        CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+        credentialRepresentation.setType(CredentialRepresentation.PASSWORD);
+        credentialRepresentation.setValue(user.password());
+        credentialRepresentation.setTemporary(false);
+        return credentialRepresentation;
     }
 
     @Override
     public void changeRole(String username, String role) {
 
+        // Implementar lógica para cambiar el rol del usuario (In progress)
+        try {
+            // Buscar usuario por nombre de usuario
+            UserRepresentation user = searchUserByUsername(username);
+            // Obtener el rol del realm
+            RoleResource roleBasic = keyService.getRealmResource().roles().get(role);
+            // Asignar el rol al usuario a nivel de realm
+            keyService.getUserResource()
+                    .get(user.getId())
+                    .roles()
+                    .realmLevel()
+                    .add(List.of(roleBasic.toRepresentation()));
+        } catch (RuntimeException e) {
+            // Registrar y lanzar cualquier excepción que ocurra
+            log.error("Error assigning role {} to user {}: {}", role, username, e.getMessage());
+            throw new RuntimeException("Error assigning role to user: " + e.getMessage(), e);
+        }
     }
 
     @Override
     public void changePassword(String idUser, String password) {
 
+//        UsersResource usersResource = keyService.getUserResource();
+//        CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+//        credentialRepresentation.setTemporary(false);
+//        credentialRepresentation.setType(CredentialRepresentation.PASSWORD);
+//        credentialRepresentation.setValue(password);
+//
+//        usersResource.get(idUser).resetPassword(credentialRepresentation);
     }
 
     @Override
     public void changeEmail(String username, String email) {
-
+        // Implementar lógica para cambiar el email del usuario si es necesario
     }
 
-    @Override
-    public void updateUser(UserRecord userRecord) {
-
-    }
 }
