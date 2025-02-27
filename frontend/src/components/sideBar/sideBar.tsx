713347abebc7abf167fe63d6581092e596558ada
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2025-02-27 20:36:54 -0300

    Update retoque de colores

--- frontend/src/components/sideBar/sideBar.tsx
+++ frontend/src/components/sideBar/sideBar.tsx
@@ -20,20 +20,20 @@ const menuItems = [
 const SideBar = () => {
   return (
     <div className="bg-[#0A0B1E] h-full w-full p-5 flex flex-col">
-      <h2 className="text-xl font-bold mb-5 text-purple-500 flex items-center">
+      <h2 className="text-xl font-bold mb-5 text-white flex items-center">
         <img
           src="https://raw.githubusercontent.com/No-Country-simulation/s21-15-n-webapp/1857a87229a6361b345560c4be845b64247ec09b/frontend/src/assets/rocket.png"
           alt="Logo"
-          className="h-6 w-6 mr-2"
+          className="h-6 w-6 mr-2 text-[#4F46E5]"
         />
         StartPerks
       </h2>
-      <ul className="text-purple-400 space-y-2 flex-1">
+      <ul className="text-[#4F46E5] space-y-2 flex-1">
         {menuItems.map((item, index) => (
           <li key={index}>
             <Link to={item.to} className="flex items-center py-2 px-4 hover:bg-[#12142A] active:bg-[#12142A] rounded">
               {item.icon}
-              {item.name}
+              <p className="text-white">{item.name}</p> 
             </Link>
           </li>
         ))}
