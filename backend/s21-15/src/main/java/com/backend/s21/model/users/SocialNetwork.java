c3eb38c65b11a2072ac1312254fe5be491c9fc6d
Omar3636 <yairmenaz92@gmail.com>
2025-02-27 22:07:46 -0300

    feat(annot): Add @AllArgsConstructor to create an object to link a User.

--- backend/s21-15/src/main/java/com/backend/s21/model/users/SocialNetwork.java
+++ backend/s21-15/src/main/java/com/backend/s21/model/users/SocialNetwork.java
@@ -1,12 +1,14 @@
 package com.backend.s21.model.users;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
 @Table(name = "social_networks")
 public class SocialNetwork {
 
