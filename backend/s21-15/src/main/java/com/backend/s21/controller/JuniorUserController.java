e62ef5cb7bb73538ba622df8a89b1fa03508e612
Omar3636 <yairmenaz92@gmail.com>
2025-03-02 22:36:46 -0300

    feat(response): Added response for bad request.

--- backend/s21-15/src/main/java/com/backend/s21/controller/JuniorUserController.java
+++ backend/s21-15/src/main/java/com/backend/s21/controller/JuniorUserController.java
@@ -6,11 +6,11 @@ import com.backend.s21.model.users.SocialNetwork;
 import com.backend.s21.model.users.User;
 import com.backend.s21.service.*;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -44,66 +44,95 @@ public class JuniorUserController {
         this.mentorshipHRepository = mentorshipHRepository;
     }
 
+    //Falta implementar el retorno del mensaje apropiado manejado en GlobalException en cada método.
     @PostMapping
-    public ResponseEntity<JuniorUserDTO> registerJuniorUser(@RequestBody @Validated JuniorUser juniorUser,
-                                                            UriComponentsBuilder uriComponentsBuilder) {
-        JuniorUser user = juniorRepository.save(juniorUser);
-        JuniorUserDTO juniorUserDto = new JuniorUserDTO(juniorUser);
-        URI url = uriComponentsBuilder.path("/junior/{id}").buildAndExpand(user.getId()).toUri();
-        return ResponseEntity.created(url).body(juniorUserDto);
+    public ResponseEntity<?> registerJuniorUser(@RequestBody @Valid JuniorUser juniorUser,
+                                                            UriComponentsBuilder uri) {
+        try {
+            JuniorUser user = juniorRepository.save(juniorUser);
+            JuniorUserDTO juniorUserDto = new JuniorUserDTO(juniorUser);
+            URI url = uri.path("/junior/{id}").buildAndExpand(user.getId()).toUri();
+            return ResponseEntity.created(url).body(juniorUserDto);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<JuniorUserDTO> showUser(@PathVariable int id) {
-        JuniorUser user = juniorRepository.findById(id);
-        return ResponseEntity.ok(new JuniorUserDTO(user));
+    public ResponseEntity<?> showUser(@PathVariable int id) {
+        try {
+            JuniorUser user = juniorRepository.findById(id);
+            return ResponseEntity.ok(new JuniorUserDTO(user));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @PostMapping("/{id}/socialnetworks")
-    public ResponseEntity<SocialNetworkDTO> linkSocialNetwork(@RequestBody @Validated SocialNetwork socialNet,
+    public ResponseEntity<?> linkSocialNetwork(@RequestBody @Valid SocialNetwork socialNet,
                                                               @PathVariable int id) {
-        User user = juniorRepository.findById(id);
-        SocialNetwork socialNetwork = socialNRepository.save(new SocialNetwork(null, user, socialNet.getName(),
-                socialNet.getUrl()));
-        return ResponseEntity.ok(new SocialNetworkDTO(socialNetwork));
+        try {
+            User user = juniorRepository.findById(id);
+            SocialNetwork socialNetwork = socialNRepository.save(new SocialNetwork(null, user, socialNet.getName(),
+                    socialNet.getUrl()));
+            return ResponseEntity.ok(new SocialNetworkDTO(socialNetwork));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @GetMapping("/{id}/challengehistory")
     public ResponseEntity<Page<ChallengeHistoryDTO>> listChallengeHistory(@PathVariable int id) {
-        JuniorUser user = juniorRepository.findById(id);
-        return ResponseEntity.ok(new PageImpl<>(user.getChallengeHistory(), Pageable.unpaged(),
-                user.getChallengeHistory().size()).map(ChallengeHistoryDTO::new));
+        try {
+            JuniorUser user = juniorRepository.findById(id);
+            return ResponseEntity.ok(new PageImpl<>(user.getChallengeHistory(), Pageable.unpaged(),
+                    user.getChallengeHistory().size()).map(ChallengeHistoryDTO::new));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping("/{id}/coursehistory")
     public ResponseEntity<Page<CourseHistoryDTO>> listCourseHistory(@PathVariable int id) {
-        JuniorUser user = juniorRepository.findById(id);
-        return ResponseEntity.ok(new PageImpl<>(user.getCourseHistory(), Pageable.unpaged(),
-                user.getCourseHistory().size()).map(CourseHistoryDTO::new));
+        try {
+            JuniorUser user = juniorRepository.findById(id);
+            return ResponseEntity.ok(new PageImpl<>(user.getCourseHistory(), Pageable.unpaged(),
+                    user.getCourseHistory().size()).map(CourseHistoryDTO::new));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @GetMapping("/{id}/mentorshiphistory")
     public ResponseEntity<Page<MentorshipHistoryDTO>> listMentorshipHistory(@PathVariable int id) {
-        JuniorUser user = juniorRepository.findById(id);
-        return ResponseEntity.ok(new PageImpl<>(user.getMentorshipHistory(), Pageable.unpaged(),
-                user.getMentorshipHistory().size()).map(MentorshipHistoryDTO::new));
+        try {
+            JuniorUser user = juniorRepository.findById(id);
+            return ResponseEntity.ok(new PageImpl<>(user.getMentorshipHistory(), Pageable.unpaged(),
+                    user.getMentorshipHistory().size()).map(MentorshipHistoryDTO::new));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<JuniorUserDTO> updateUser(@RequestBody @Validated JuniorUser userJson, @PathVariable int id) {
+    public ResponseEntity<?> updateUser(@RequestBody @Valid JuniorUser userJson, @PathVariable int id) {
         try {
             JuniorUser user = juniorRepository.update(userJson, id);
             return ResponseEntity.ok(new JuniorUserDTO(user));
-        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException | RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public ResponseEntity deleteUser(@PathVariable int id) {
-        JuniorUser user = juniorRepository.findById(id);
-        user.setDeleted(true);
-        return ResponseEntity.ok("El usuario ha sido eliminado con exito.");
+    public ResponseEntity<?> deleteUser(@PathVariable int id) {
+        try {
+            JuniorUser user = juniorRepository.findById(id);
+            user.setDeleted(true);
+            return ResponseEntity.ok("El usuario ha sido eliminado con exito.");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
