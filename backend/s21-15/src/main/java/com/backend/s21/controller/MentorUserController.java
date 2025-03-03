d95908c28a6d2a65181fc5fc31bdcf0e2111eb1e
Omar3636 <yairmenaz92@gmail.com>
2025-03-03 20:41:36 -0300

    feat(response): Added bad request response and mentorship list linked to a MentorUser.

--- backend/s21-15/src/main/java/com/backend/s21/controller/MentorUserController.java
+++ backend/s21-15/src/main/java/com/backend/s21/controller/MentorUserController.java
@@ -1,11 +1,9 @@
 package com.backend.s21.controller;
 
 import com.backend.s21.model.dto.CourseDTO;
-import com.backend.s21.model.dto.JuniorUserDTO;
 import com.backend.s21.model.dto.MentorshipDTO;
 import com.backend.s21.model.learningPath.Course;
 import com.backend.s21.model.learningPath.Mentorship;
-import com.backend.s21.model.users.JuniorUser;
 import com.backend.s21.model.users.MentorUser;
 import com.backend.s21.model.users.User;
 import com.backend.s21.service.ICourseService;
@@ -28,72 +26,90 @@ public class MentorUserController {
 
     private final IMentorUserService mentorRepository;
 
-    private final ICourseService ICourseRepository;
+    private final ICourseService courseRepository;
 
-    private final IMentorshipService IMentorshipRepository;
+    private final IMentorshipService mentorshipRepository;
 
-    public MentorUserController(IMentorUserService mentorRepository, ICourseService ICourseRepository,
-                                IMentorshipService IMentorshipRepository) {
+    public MentorUserController(IMentorUserService mentorRepository, ICourseService courseRepository,
+                                IMentorshipService mentorshipRepository) {
         this.mentorRepository = mentorRepository;
-        this.ICourseRepository = ICourseRepository;
-        this.IMentorshipRepository = IMentorshipRepository;
+        this.courseRepository = courseRepository;
+        this.mentorshipRepository = mentorshipRepository;
     }
 
     @PostMapping
-    public ResponseEntity<MentorUser> registerMentorUser(@RequestBody @Validated MentorUser userJson,
+    public ResponseEntity<?> registerMentorUser(@RequestBody @Validated MentorUser userJson,
                                                          UriComponentsBuilder uri) {
-        MentorUser user = mentorRepository.save(userJson);
-        URI url = uri.path("/mentor/{nickname}").buildAndExpand(user.getNickname()).toUri();
-        return ResponseEntity.created(url).body(user);
+        try {
+            MentorUser user = mentorRepository.save(userJson);
+            URI url = uri.path("/mentor/{nickname}").buildAndExpand(user.getNickname()).toUri();
+            return ResponseEntity.created(url).body(user);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
-
     @GetMapping("/{id}")
-    public ResponseEntity<MentorUser> showUser(@PathVariable int id) {
-        MentorUser user = mentorRepository.findById(id);
-        return ResponseEntity.ok(user);
+    public ResponseEntity<?> showUser(@PathVariable int id) {
+        try {
+            MentorUser user = mentorRepository.findById(id);
+            return ResponseEntity.ok(user);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @PostMapping("/{id}/createcourse")
     @Transactional
-    public ResponseEntity<CourseDTO> createCourse(@RequestBody @Validated Course courseinfo, @PathVariable int id,
+    public ResponseEntity<?> createCourse(@RequestBody @Validated Course courseJson, @PathVariable int id,
                                                   UriComponentsBuilder uri) {
-        Course course = ICourseRepository.save(courseinfo);
-        User instructor = mentorRepository.findById(id);
-        course.setInstructor(instructor);
-        CourseDTO courseDTO = new CourseDTO(course);
-        URI url = uri.path("/course/{id}").buildAndExpand(courseDTO.getId()).toUri();
-        return ResponseEntity.created(url).body(courseDTO);
+        try {
+            Course course = courseRepository.save(courseJson);
+            User instructor = mentorRepository.findById(id);
+            course.setInstructor(instructor);
+            CourseDTO courseDTO = new CourseDTO(course);
+            URI url = uri.path("/course/{id}").buildAndExpand(courseDTO.getId()).toUri();
+            return ResponseEntity.created(url).body(courseDTO);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @GetMapping("/{id}/yourcourselist")
     public ResponseEntity<Page<CourseDTO>> listCoursesFromMentor(@PathVariable int id) {
-        User user = mentorRepository.findById(id);
-        Page<Course> listCourses = ICourseRepository.findByInstructorId(Pageable.unpaged(), user.getId());
-
-        return ResponseEntity.ok(listCourses.map(CourseDTO::new));
+        try {
+            User user = mentorRepository.findById(id);
+            Page<Course> listCourses = courseRepository.findByInstructorId(Pageable.unpaged(), user.getId());
+            return ResponseEntity.ok(listCourses.map(CourseDTO::new));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/{id}/creatementorship")
     @Transactional
-    public ResponseEntity<MentorshipDTO> createMentorship(@PathVariable int id,
+    public ResponseEntity<?> createMentorship(@PathVariable int id,
                                                           @RequestBody @Validated Mentorship mentorshipJson,
                                                           UriComponentsBuilder uri) {
-        MentorUser mentor = mentorRepository.findById(id);
-        Mentorship mentorship = new Mentorship(null, mentor, mentorshipJson.getTitle(),
-                mentorshipJson.getDescription(), null, null);
-        IMentorshipRepository.save(mentorship);
-        URI url = uri.path("/mentorship/{id}").buildAndExpand(mentorship.getId()).toUri();
-        return ResponseEntity.created(url).body(new MentorshipDTO(mentorship));
+        try {
+            MentorUser mentor = mentorRepository.findById(id);
+            Mentorship mentorship = new Mentorship(null, mentor, mentorshipJson.getTitle(),
+                    mentorshipJson.getDescription(), null, null);
+            mentorshipRepository.save(mentorship);
+            URI url = uri.path("/mentorship/{id}").buildAndExpand(mentorship.getId()).toUri();
+            return ResponseEntity.created(url).body(new MentorshipDTO(mentorship));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<MentorUser> updateUser(@RequestBody @Validated MentorUser userJson, @PathVariable int id) {
+    public ResponseEntity<?> updateUser(@RequestBody @Validated MentorUser userJson, @PathVariable int id) {
         try {
             MentorUser user = mentorRepository.update(userJson, id);
             return ResponseEntity.ok(user);
         } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
@@ -103,6 +119,17 @@ public class MentorUserController {
         MentorUser user = mentorRepository.findById(id);
         user.setDeleted(true);
         return ResponseEntity.ok("El usuario ha sido eliminado con exito.");
+    }
+
+    @GetMapping("/{id}/yourmentorshiplist")
+    public ResponseEntity<Page<MentorshipDTO>> listMentorshipFromMentor(@PathVariable int id, Pageable pageable) {
+        try {
+            MentorUser user = mentorRepository.findById(id);
+            Page<Mentorship> mentorshipList = mentorshipRepository.findByMentorId(user.getId(), pageable);
+            return ResponseEntity.ok(mentorshipList.map(MentorshipDTO::new));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
