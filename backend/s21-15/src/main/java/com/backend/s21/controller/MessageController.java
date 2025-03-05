f684e53649c56ce5ec7e1d7814a3b383623d6f8a
Jose Lambrechts <alt165@gmail.com>
2025-03-05 18:26:04 -0300

    feat: message system implementation.

---
+++ backend/s21-15/src/main/java/com/backend/s21/controller/MessageController.java
@@ -0,0 +1,37 @@
+package com.backend.s21.controller;
+
+import com.backend.s21.model.message.Message;
+import com.backend.s21.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/send")
+    public ResponseEntity<Message> sendMessage(
+            String user,
+            @RequestParam String receiverUsername,
+            @RequestParam String content) {
+
+        Message message = messageService.sendMessage(user, receiverUsername, content);
+        return ResponseEntity.ok(message);
+    }
+    @GetMapping("/sent")
+    public ResponseEntity<List<Message>> getSentMessages(@Validated String user) {
+        return ResponseEntity.ok(messageService.getSentMessages(user));
+    }
+
+    @GetMapping("/received")
+    public ResponseEntity<List<Message>> getReceivedMessages(@Validated String user) {
+        return ResponseEntity.ok(messageService.getReceivedMessages(user));
+    }
+}
---
+++ backend/s21-15/src/main/resources/db/migration/V5__create-table-messages.sql
@@ -0,0 +1,12 @@
+-- -----------------------------------------------------
+-- Table `s2115n`.`messages`
+-- -----------------------------------------------------
+CREATE TABLE messages (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    content TEXT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
+);
